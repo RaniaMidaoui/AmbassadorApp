7f3d70ec7cda1d3872da20adf31a19e793427873
Mohamed Sadok Masghouni <94572086+SADOOK7@users.noreply.github.com>
2023-05-28 01:27:20 +0100

    Update constants.ts

--- Ambassador_App_Frontend-main/Ambassador_App_Frontend-main/next-checkout/constants.ts
+++ Ambassador_App_Frontend-main/Ambassador_App_Frontend-main/next-checkout/constants.ts
@@ -1,4 +1,5 @@
 export default {
-    endpoint: 'http://localhost:8000/api/checkout',
+    // endpoint: 'http://localhost:8000/api/checkout',
+    endpoint: '/api/checkout',
     stripe_key: 'pk_test_51MX0VwKGOOiy0u9Jl0xQzCLiu11zUHGWim6J0LE9rsvOTE4mUimHUDehevHuQbYPjEqf0BA29UOINtO24peNfsvY00MbaWdpXL'
 }
