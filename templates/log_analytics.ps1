932ad99026f4b294d59a360c01ef32aa83cf39c0
Rania Midaoui <68945305+RaniaMidaoui@users.noreply.github.com>
2023-12-23 13:41:18 +0100

    added jenkins CI

--- templates/log_analytics.ps1
+++ templates/log_analytics.ps1
@@ -4,7 +4,7 @@ $LOCATION='northeurope'
 $NODE_SIZE='Standard_B2s'
 $DEPLOYMENT_NAME='projectcluster'
 $SUBSCRIPTION_NAME='AzureForStudents'
-$CUSTOM_DOMAIN='ambassamart.store'
+$CUSTOM_DOMAIN='ambassamart.shop'
 $INGRESS_NAMESPCAE='default'
 $LOG_ANALY_NAME='ambassamart'
 
@@ -14,10 +14,10 @@ $LOG_ANALY_ID=$(az monitor log-analytics workspace create --resource-group $RESO
 #az aks create --resource-group $RESOURCE_GROUP_NAM --name $AKS_NAME --node-count 2 --enable-addons monitoring --workspace-resource-id $LOG_ANALY_ID
 
 # Get resource group ID
-$RG_ID=$(az group show --name  pfarg --query id --output tsv)
+$RG_ID=$(az group show --name  projectrg --query id --output tsv)
 
 # Create a service principal and assign Log Analytics Reader role to the resource group
-az ad sp create-for-rbac --role="Log Analytics Reader" --scopes="/subscriptions/5dfcf723-ea7b-4a89-b344-86968954d351/resourceGroups/pfarg"
+az ad sp create-for-rbac --role="Log Analytics Reader" --scopes="/subscriptions/5dfcf723-ea7b-4a89-b344-86968954d351/resourceGroups/projectrg"
 
 # Add Grafana repo
 helm repo add grafana https://grafana.github.io/helm-charts
@@ -30,8 +30,8 @@ kubectl create namespace monitoring
 helm install stable grafana/grafana -n monitoring
 kubectl get pods -n monitoring
 
-# Expose Grafana, port forward to the Grafana Service
-kubectl port-forward -n monitoring stable-grafana-54f4f99789-mtb97 3000
+# # Expose Grafana, port forward to the Grafana Service
+kubectl port-forward -n monitoring stable-grafana-7fbf548db9-h94rt 3000
 
 # Get Grafana admin password (base64 format, needs to be decoded) #10956
 kubectl get secret --namespace monitoring stable-grafana -o jsonpath="{.data.admin-password}"
